7710592383deec13d7ab790e8930bb61b456805e
hdpklm <hdaoud10@gmail.com>
2021-10-16 17:39:14 +0200

    change settings for database

--- build/server/api/settings/settings.php
+++ build/server/api/settings/settings.php
@@ -1,13 +1,13 @@
 <?php
 $dbHost = 'localhost';
 $dbName = 'favori';
-$dbusername = 'root';
-$dbpassword = '';
+$dbusername = 'dark';
+$dbpassword = 'dark123*';
 
 $jwt_secret = 'secret_key';
 
 $upload_directory = '../media/';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && empty($_REQUEST)) {
-	$_REQUEST = json_decode(file_get_contents('php://input'), true);
+    $_REQUEST = json_decode(file_get_contents('php://input'), true);
 }
