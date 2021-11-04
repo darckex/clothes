844343c82910b2514566e0fcf6f12dc6278b95da
darckex <darcksx@gmail.com>
2021-11-04 11:16:35 +0200

    removed https

--- src/settings.js
+++ src/settings.js
@@ -1,2 +1,2 @@
 // export const server = "http://localhost/work/clothes/public/server"
-export const server = "https://www.favorilb.com/server"
+export const server = "http://www.favorilb.com/server"
